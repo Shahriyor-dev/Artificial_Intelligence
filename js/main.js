f8963b0fed20097c7f49ff809fa5a5e0a47a9c7a
Shahriyor-dev <burxonovshahriyor@gmail.com>
2021-06-16 15:57:34 +0500

    fixed: adaptived slider

--- js/main.js
+++ js/main.js
@@ -5,4 +5,26 @@ const swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
   },
+    breakpoints: {
+    1100: {
+      slidesPerView: 3,
+      spaceBetween:0,
+    },
+    700:{
+      slidesPerView:2,
+      spaceBetween:0,
+    },
+    500:{
+      sliderPerView:2,
+    },
+    600:{
+      slidesPerView:1,
+    },
+    400:{
+      slidesPerView:1,
+    },
+    320:{
+      slidesPerView:1,
+    },
+  }
 });
